9827c0caffe626945d1287b1334a0456e8f24c7a
Maarten de Vries <maarten@de-vri.es>
2023-08-04 23:00:39 +0200

    Fix example.

--- examples/username-password.rs
+++ examples/username-password.rs
@@ -1,5 +1,5 @@
 fn main() -> std::io::Result<()> {
-	let mut terminal = terminal_echo::TerminalPrompter::open()?;
+	let mut terminal = terminal_prompt::TerminalPrompter::open()?;
 	let username = terminal.prompt("Username: ")?;
 	let password = terminal.prompt_sensitive("Password: ")?;
 	println!("Username: {username}");
